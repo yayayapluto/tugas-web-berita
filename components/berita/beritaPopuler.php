755111c14fdd90b0b22fa7d1dde1c7ea7086f364
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-05-31 09:36:57 +0700

    Fix style on beritaTerbaru

--- components/berita/beritaPopuler.php
+++ components/berita/beritaPopuler.php
@@ -8,7 +8,7 @@
                     <img src="<?= $berita['url_gambar'] ?>"
                         class="w-full h-48 object-cover rounded-t-lg " alt="<?= "Gambar untuk judul: " . str_replace("-"," ",$berita['judul'])?>">
                     <div class="px-4 py-2">
-                        <h3 class="text-lg font-bold mb-2"><?= str_replace("-"," ",$berita['judul'])?></h3>
+                        <h3 class="text-lg font-bold mb-2 text-ellipsis text-nowrap overflow-hidden lg:w-full"><?= str_replace("-"," ",$berita['judul'])?></h3>
                         <p class="text-gray-700 mb-2 text-ellipsis text-nowrap overflow-hidden w-20  lg:w-72"><?= $berita['deskripsi'] ?></p>
                         <div class="flex justify-between items-center">
                             <p class="text-sm text-gray-500">Dibuat oleh <?= $berita['penulis'] ?> - <?= $berita['waktu_dibuat'] ?></p>
--- components/berita/beritaTerbaru.php
+++ components/berita/beritaTerbaru.php
@@ -7,7 +7,7 @@
               <div class="flex items-center p-4 bg-white rounded shadow hover:shadow-md">
                 <img src="<?= $berita['url_gambar'] ?>" class="w-48 h-24 object-cover rounded-lg mr-4" alt="<?= "Gambar untuk judul: " . str_replace("-"," ",$berita['judul'])?>">
                 <div class="flex-grow">
-                  <h4 class="text-md font-bold mb-1"><?= str_replace("-"," ",$berita['judul'])?></h4>
+                  <h4 class="text-md font-bold mb-1 text-ellipsis text-nowrap overflow-hidden w-40 lg:w-96"><?= str_replace("-"," ",$berita['judul'])?></h4>
                   <p class="text-gray-700 text-sm mb-2 text-ellipsis text-nowrap overflow-hidden w-20  lg:w-64"><?= $berita['deskripsi'] ?></p>
                   <a href="/web_berita/baca/<?= $berita['judul']?>" class="py-1 px-3 bg-blue-600 rounded-sm text-white hover:shadow-lg shadow-md">Read More</a>
                 </div>
